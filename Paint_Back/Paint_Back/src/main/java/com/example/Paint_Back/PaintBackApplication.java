3b0070b59a477b7fb313f0772f3f4ce87537b586
Random <ehabflash833@gmail.com>
2022-12-02 11:04:00 +0200

    add

---
+++ Paint_Back/Paint_Back/src/main/java/com/example/Paint_Back/PaintBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.Paint_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintBackApplication.class, args);
+	}
+
+}
---
+++ Paint_Back/Paint_Back/src/test/java/com/example/Paint_Back/PaintBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Paint_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
