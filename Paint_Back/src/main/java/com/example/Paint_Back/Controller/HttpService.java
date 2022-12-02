d10ff5c77038eef2737a949b05df0ac2bc0877a5
Random <ehabflash833@gmail.com>
2022-12-02 11:19:43 +0200

    add

---
+++ Paint_Back/src/main/java/com/example/Paint_Back/Controller/HttpService.java
@@ -0,0 +1,14 @@
+package com.example.Paint_Back.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpService {
+
+    @GetMapping(value = "/")
+    public String str () {
+        return "Hello there!";
+    }
+
+}
